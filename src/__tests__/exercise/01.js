1867b7a9a3b4cc031fa0f3044f2c1a00b26bdcee
Odysseus <64851813+NathanGanesh@users.noreply.github.com>
2021-03-11 18:32:19 +0100

    first commit

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -1,29 +1,38 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
-import * as React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from '../../components/counter'
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from '../../components/counter';
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use ReactDOM.render to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 increment.click())
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 decrement.click())
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
-})
+	// 🐨 create a div to render your component to (💰 document.createElement)
+	const root = document.createElement('div');
+	// 🐨 append the div to document.body (💰 document.body.append)
+	document.body.append(root);
+	// 🐨 use ReactDOM.render to render the <Counter /> to the div
+	ReactDOM.render(<Counter />, root);
+	const message = root.firstChild.querySelector('div');
+	console.log(message.textContent);
+	// 🐨 get a reference to the increment and decrement buttons:
+	//   💰 div.querySelectorAll('button')
+	// const { decrement, increment } = root.querySelectorAll('button');
+	// // 🐨 get a reference to the message div:
+	// //   💰 div.firstChild.querySelector('div')
+
+	// // 🐨 expect the message.textContent toBe 'Current count: 0'
+	// expect(root.firstChild.querySelectorAll('div').textContent, 'Current count: 0');
+	// // 🐨 click the increment button (💰 increment.click())
+	// increment.click();
+	// // 🐨 assert the message.textContent
+	// expect(root.firstChild.querySelectorAll('div').textContent, 'Current count: 1');
+	// // 🐨 click the decrement button (💰 decrement.click())
+	// decrement.click();
+	// // 🐨 assert the message.textContent
+	// expect(root.firstChild.querySelectorAll('div').textContent, 'Current count: 0');
+	// // 🐨 cleanup by removing the div from the page (💰 div.remove())
+	// root.remove();
+	// 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+});
 
 /* eslint no-unused-vars:0 */
--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -1,21 +1,23 @@
 // Avoid implementation details
 // http://localhost:3000/counter
 
-import * as React from 'react'
+import * as React from 'react';
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
-import Counter from '../../components/counter'
+import { render, screen } from '@testing-library/react';
+import Counter from '../../components/counter';
+import userEvent from '@testing-library/user-event';
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+	const { container } = render(<Counter />);
+	// 🐨 replace these with screen queries
+	// 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
+	const decrement = screen.getByText('Decrement');
+	const increment = screen.getByText('Increment');
+	const message = screen.getByText(/current count/i);
 
-  expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
-})
+	expect(message).toHaveTextContent('Current count: 0');
+	userEvent.click(increment);
+	expect(message).toHaveTextContent('Current count: 1');
+	userEvent.click(decrement);
+	expect(message).toHaveTextContent('Current count: 0');
+});
